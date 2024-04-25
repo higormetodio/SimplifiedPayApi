c75436a42bda3e346bb26a80b6db86dff138e765
Higor Metodio <higor.metodio@outlook.com>
2024-04-25 17:42:33 -0300

    Adjustments to the description of the IdentificationNumber

--- SimplifiedPayApi/SimplifiedPayApi/Models/User.cs
+++ SimplifiedPayApi/SimplifiedPayApi/Models/User.cs
@@ -6,7 +6,7 @@ public class User
 {
     public int Id { get; set; }
     public string? FullName { get; set; }
-    public string? IdentificationNumebr { get; set; }
+    public string? IdentificationNumber { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
     public UserType UserType { get; set; }
