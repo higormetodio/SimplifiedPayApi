f28d406daae12acf1d5416868471096a0ddaebd7
Higor Metodio <higor.metodio@outlook.com>
2024-04-24 21:42:38 -0300

    add navigation properties

--- SimplifiedPayApi/SimplifiedPayApi/Models/Account.cs
+++ SimplifiedPayApi/SimplifiedPayApi/Models/Account.cs
@@ -5,5 +5,6 @@ public class Account
     public int Id { get; set; }
     public decimal Amount { get; set; }
     public User? User { get; set; }
+    public int UserId { get; set; }
     public DateTime Timestamp { get; set; }
 }
--- SimplifiedPayApi/SimplifiedPayApi/Models/Transaction.cs
+++ SimplifiedPayApi/SimplifiedPayApi/Models/Transaction.cs
@@ -5,7 +5,9 @@ public class Transaction
     public int Id { get; set; }
     public decimal Amount { get; set; }
     public User? Payer { get; set; }
+    public int PayerId { get; set; }
     public User? Receiver { get; set; }
+    public int ReceiverId { get; set; }
     public bool Status { get; set; }
     public DateTime Timestamp { get; set; }
 }
