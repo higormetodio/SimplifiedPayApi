6fb2a4c05d848abd43d1dc5fdc8fbb9404cf150e
Higor Metodio <higor.metodio@outlook.com>
2024-04-25 14:50:47 -0300

    changing the mapping of the entities in a separate class

---
+++ SimplifiedPayApi/SimplifiedPayApi/Database/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using SimplifiedPayApi.Models;
+
+namespace SimplifiedPayApi.Context;
+
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Deposit> Accounts { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+
+
+    }
+}
